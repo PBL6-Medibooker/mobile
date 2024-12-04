1f94acdb0d85d3d6b5806693d98b3743f0f6b00a
HueLe <lehue1802.ht@gmail.com>
2024-12-04 16:23:54 +0700

    4/12

--- src/API/client.js
+++ src/API/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.11:4000",
-  // baseURL: "https://backend-6jva.onrender.com"
+  // baseURL: "http://192.168.1.11:4000",
+  baseURL: "https://medi-booker-backend.onrender.com"
   // headers: {
   //   "Content-Type": "application/json",
   // },
